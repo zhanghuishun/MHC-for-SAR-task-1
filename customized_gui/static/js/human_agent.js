5857558786d065f8e0b5b046955b55b3514823e0
Huishun Zhang <345892489@qq.com>
2022-03-07 10:37:59 +0100

    add hidden sidebar

--- customized_gui/static/js/human_agent.js
+++ customized_gui/static/js/human_agent.js
@@ -25,3 +25,32 @@ function checkArrowKey(e) {
 
     send_userinput_to_MATRX(data);
 }
+
+var showing_sidebar = false
+function moralvalueToggle() {
+    showing_sidebar = !showing_sidebar;
+    if (showing_sidebar) {
+        document.getElementById("moral_value_button").className = "btn btn-secondary";
+    } else {
+        document.getElementById("moral_value_button").className = "btn btn-dark";
+    }
+}
+
+$(document).ready(function(){
+    $("#reset").click(function(){
+      $("#copy-list").empty();
+            });
+  });
+sortable('.js-sortable-copy', {
+forcePlaceholderSize: true,
+copy: true,
+      acceptFrom: false,
+placeholderClass: 'mb1 bg-navy border border-yellow',
+});
+sortable('.js-sortable-copy-target', {
+  forcePlaceholderSize: true,
+  acceptFrom: '.js-sortable-copy,.js-sortable-copy-target',
+  placeholderClass: 'mb1 border border-maroon',
+  maxItems: 5
+});
+
